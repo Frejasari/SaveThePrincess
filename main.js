47ea8064dfabd4d249d36c0f79a4037383bc6216
Sari <freja.sender@gmail.com>
2018-06-04 14:53:23 +0200

    create main.js
    + add keydown listener to move bomberman

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 /// just for testing at the moment!
 
-var test = document.getElementById("bomberman");
+var bombermanTest = document.getElementById("bomberman");
 var game = new BombermanGame();
+
+document.addEventListener("keydown", function(event) {
+  console.log("key pressed! Event ", event);
+  switch (event.key) {
+    case "ArrowDown":
+      game.moveBomberman(DIRECTION_ENUM.SOUTH);
+      bombermanTest.style.top = game.bomberman.y + "px";
+      break;
+    case "ArrowUp":
+      game.moveBomberman(DIRECTION_ENUM.NORTH);
+      bombermanTest.style.top = game.bomberman.y + "px";
+      break;
+    case "ArrowRight":
+      game.moveBomberman(DIRECTION_ENUM.EAST);
+      bombermanTest.style.left = game.bomberman.x + "px";
+      break;
+    case "ArrowLeft":
+      game.moveBomberman(DIRECTION_ENUM.WEST);
+      bombermanTest.style.left = game.bomberman.x + "px";
+      break;
+  }
+});
